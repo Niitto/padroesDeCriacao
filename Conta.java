075df7eda5a343ceba7012134e1beffead0ca056
Niitto <103511960+Niitto@users.noreply.github.com>
2022-10-11 20:10:05 -0300

    second commit

---
+++ Conta.java
@@ -0,0 +1,72 @@
+public class Conta{
+    public enum Categoria{NORMAL, ADVANCED, PREMIUM}
+
+    private long numero;
+    private String nomeCorrentista;
+    private int saldoLivreInicial;
+    private int salAplicacaoInicial;
+    private double taxaRemuneracao;
+    private double taxaSaldoNegativo;
+    private Categoria categoriaInicial;
+
+    public static class Builder{
+            private long numero;
+            private String nomeCorrentista;
+            private int saldoLivreInicial;
+            private int salAplicacaoInicial;
+            private double taxaRemuneracao;
+            private double taxaSaldoNegativo;
+            private Categoria categoriaInicial;  
+
+        public Builder() {
+        }
+
+        public Builder categoria(Conta.Categoria categoriaInicial) {
+            this.categoriaInicial = categoriaInicial;
+            return this;
+        }
+        public Builder numero(){
+            this.numero = numero;
+            return this;
+        }
+        public Builder nome(){
+            this.nomeCorrentista = nomeCorrentista;
+            return this;
+        }
+        public Builder saldoLivre(){
+            this.saldoLivreInicial = saldoLivreInicial;
+            return this;
+        }
+        public Builder saldoAplicacao(){
+            this.salAplicacaoInicial = salAplicacaoInicial;
+            return this;
+        }
+        public Builder taxaRemuneracao(){
+            this.taxaRemuneracao = taxaRemuneracao;
+            return this;
+        }
+        public Builder taxaSaldoNegativo(){
+            this.taxaSaldoNegativo = taxaSaldoNegativo;
+            return this;
+        }
+        public Builder categoria(){
+            this.categoriaInicial = categoriaInicial;
+            return this;
+        }
+        public Conta build(){
+            return new Conta(this);
+        }
+    }
+
+    private Conta(Builder builder) {
+        this.categoriaInicial = builder.categoriaInicial;
+        this.numero = builder.numero;
+        this.nomeCorrentista = builder.nomeCorrentista;
+        this.saldoLivreInicial = builder.saldoLivreInicial;
+        this.salAplicacaoInicial = builder.salAplicacaoInicial;
+        this.taxaRemuneracao = builder.taxaRemuneracao;
+        this.taxaSaldoNegativo = builder.taxaSaldoNegativo;
+        this.categoriaInicial = builder.categoriaInicial;
+
+    }
+}
